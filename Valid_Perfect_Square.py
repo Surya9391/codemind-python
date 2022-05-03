3f1564680ef47b7f10e5770ab84465587a9b9469
Surya9391 <97425284+Surya9391@users.noreply.github.com>
2022-05-03 16:18:56 +0530

    import math def is_valid(num):     sq=int(math.pow(num,0.5))     if sq**2==num:         return True     else:         return False
    n=int(input())
    for i in range(n):
        num=int(input())
        answer=is_valid(num)
        print(answer)

---
+++ Valid_Perfect_Square.py
@@ -0,0 +1,14 @@
+import math
+def is_valid(num):
+    sq=int(math.pow(num,0.5))
+    if sq**2==num:
+        return True
+    else:
+        return False
+        
+n=int(input())
+for i in range(n):
+    num=int(input())
+    answer=is_valid(num)
+    print(answer)
+        
