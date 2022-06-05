0972afe5bbe6123e471673643fb4c6214b94c2d5
Surya9391 <97425284+Surya9391@users.noreply.github.com>
2022-06-05 19:43:04 +0530

    a,b=map(int,input().split()) l=max(a,b) c=abs(a-b) if(c==1 or c==-1):     print("Yes") elif(l==0 and c==9 or c==-9):     print("Yes") elif(l==10 and c==9 or c==-9):     print("Yes") else:     print("No")

---
+++ Edge_Checker.py
@@ -0,0 +1,11 @@
+a,b=map(int,input().split())
+l=max(a,b)
+c=abs(a-b)
+if(c==1 or c==-1):
+    print("Yes")
+elif(l==0 and c==9 or c==-9):
+    print("Yes")
+elif(l==10 and c==9 or c==-9):
+    print("Yes")
+else:
+    print("No")
