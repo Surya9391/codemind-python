2d8ec1b62024d5ee9efb3a7b3a64f47d986ca726
Surya9391 <97425284+Surya9391@users.noreply.github.com>
2022-06-11 01:02:56 +0530

    n=int(input()) Sum=0 temp=n while(n!=0):     d=n%10     Sum=Sum*10+d     n=n//10 n=temp if(Sum==n):     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,12 +1,12 @@
 n=int(input())
-rev=0
+Sum=0
 temp=n
 while(n!=0):
     d=n%10
-    rev=(rev*10)+d
+    Sum=Sum*10+d
     n=n//10
 n=temp
-if rev==n:
-    print('True')
+if(Sum==n):
+    print("True")
 else:
-    print('False')
+    print("False")
